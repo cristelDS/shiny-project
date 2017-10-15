a0849d9272be83f2674bf2dda24c61c3526103ae
Cristel <cristelDS@users.noreply.github.com>
2017-10-15 22:29:17 +0100

    remove unnecessary code

--- MDGproject/server.R
+++ MDGproject/server.R
@@ -5,7 +5,7 @@ library(shiny)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-  #output$selecyear <- renderText(paste("you selected the value", input$slideyear))
+  
   modified_date <- max(file.info(dir())[4][,1])
   output$now <- renderText(paste("This page was last updated on ", modified_date))
   
@@ -16,15 +16,7 @@ shinyServer(function(input, output) {
       ggplot(aes(serieCode,indicator, fill = serieName)) +
           geom_boxplot(outlier.colour = "seagreen", outlier.size = 1) +
           geom_jitter(size = 1) +
-      # outlier.colour = "#1F3552", outlier.shape = 20
-          # ylab(title(name = "Indicators") +
-          # scale_x_discrete(NULL) +
           ggtitle(coln)
-          #fill=serieCode, 
-          # main=coln,
-          # xlab="MDGs", 
-          # ylab="indicators",
-          # colour=serieCode)
   })
   })
 
