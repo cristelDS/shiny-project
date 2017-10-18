8f5f373744c4f22ffad19f851560875d6a345423
Cristel <cristel50@gmail.com>
2017-10-18 10:48:32 +0100

    aesthetic changes to plot

--- MDGproject/server.R
+++ MDGproject/server.R
@@ -6,9 +6,11 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
+  #display date of last update 
   modified_date <- max(file.info(dir())[4][,1])
   output$now <- renderText(paste("This page was last updated on ", modified_date))
   
+  #plot data 
   output$myplot <- renderPlot({
     coln <- input$slideyear
     dat1 %>% 
@@ -16,7 +18,18 @@ shinyServer(function(input, output) {
       ggplot(aes(serieCode,indicator, fill = serieName)) +
           geom_boxplot(outlier.colour = "seagreen", outlier.size = 1) +
           geom_jitter(size = 1) +
-          ggtitle(coln)
+          ggtitle(coln) + 
+          stat_summary(geom = "crossbar", width=0.65, 
+                       fatten=0, color="white", 
+                       fun.data = function(x){ return(c(y=median(x)
+                                                        , ymin=median(x)
+                                                        , ymax=median(x))) }) +
+          #legend.title=element_blank() + 
+          #panel.grid.major.x=element_blank() +
+          theme_set(theme_minimal()) +    
+          theme(axis.title.y = element_blank()) +
+          scale_y_continuous(breaks=seq(0, 1, 0.1)) +
+          xlab("Indicators")
   })
   })
 
